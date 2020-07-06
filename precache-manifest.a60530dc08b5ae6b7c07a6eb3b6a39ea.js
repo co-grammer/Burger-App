45c2abaf9bd5db25802a6e0acc5d9c00287c054e
vikas rohra <vikasrohra2@gmail.com>
2020-07-07 00:03:07 +0530

    Updates

---
+++ precache-manifest.a60530dc08b5ae6b7c07a6eb3b6a39ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0acfb14c4ba0bbf1c40e5df9f99e432",
+    "url": "/Burger-App/index.html"
+  },
+  {
+    "revision": "ba824fafe201f76adfcf",
+    "url": "/Burger-App/static/css/main.6c8da95a.chunk.css"
+  },
+  {
+    "revision": "efcbdb255cbc6c6085ab",
+    "url": "/Burger-App/static/js/2.ffb54336.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Burger-App/static/js/2.ffb54336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba824fafe201f76adfcf",
+    "url": "/Burger-App/static/js/main.8a2ed860.chunk.js"
+  },
+  {
+    "revision": "ad512ca40ee7e0b39d1f",
+    "url": "/Burger-App/static/js/runtime-main.001ccf8d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-App/static/media/burger-logo.b8503d26.png"
+  }
+]);
